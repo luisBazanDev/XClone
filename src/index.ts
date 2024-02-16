1d3b3f03e235a819464a54757ff1bacfeb682bda
Luis Bazan <luis@bazan.pe>
2024-02-16 11:16:08 -0500

    JWTMiddleware

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import { Hono } from "hono";
 import { sign } from "hono/jwt";
+import JWTMiddlewere from "./middlewares/JWTMiddlewere";
+import { setCookie } from "hono/cookie";
+import { JWT_SECRET } from "./config";
 
 const app = new Hono();
 
-app.get("/:text", async (c) => {
+app.get("/login/:text", async (c) => {
+  const token = await sign({ text: c.req.param("text") }, JWT_SECRET);
+  setCookie(c, "token", token);
   return c.json({
-    token: await sign({ text: c.req.param("text") }, "jas"),
+    token: token,
     env_test: process.env.test,
+  });
+});
+
+app.get("/*", JWTMiddlewere(), async (c) => {
+  return c.json({
+    hello: "world",
   });
 });
 
